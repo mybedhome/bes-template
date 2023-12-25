abcb05edc737f150f73fafec40b4518708d52aa6
wuhan <hwu@eppei.com>
2023-12-26 01:39:02 +0800

    更新配置

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -20,3 +20,28 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.png' {
+  const content: any
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: any
+  export default content
+}
+
+declare module '*.css' {
+  const content: any
+  export default content
+}
+
+declare module '*.scss' {
+  const content: any
+  export default content
+}
